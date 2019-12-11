521179e17de3ecfc24549262d8174affd65690aa
Jerng <yangjerng@gmail.com>
2019-12-11 20:18:49 +0800

    Implemented Node, Node.name, and interface for Pid and Node.spawn()

--- serl.js
+++ serl.js
@@ -1,13 +1,30 @@
 /* serl.js should be serl.mjs - but the lousy dev server doesn't serve the right
  * mime type 
+
  */
 
 'use strict'
 
 // This is chosen over an (export default) expression for explicitness:
-export class Serl {
+export class Node {
+    constructor (given) {
+        this.host   =   'placeholderHost'
+        this.name   =   `${given}@${this.host}`   // TODO: depends on -name vs -sname flags
 
+        /*  Distributed Erlang functionality (we don't need this yet) 
+         *  https://erlang.org/doc/reference_manual/distributed.html
+
+        let cookie  =   'placeholderCookie'
+        let hidden  =   false
+
+         */
+
+        this.spawn  = () => {}
+    }
+}
+
+export class Pid {
     constructor () {
     }
-    
 }
+
--- tests.js
+++ tests.js
@@ -1,6 +1,22 @@
-import { Serl } from './serl.js'
+import  {   Node, 
+            Pid
 
-console.log ( `--TEST #1 : does the Serl class / function / object exist?` )
-try { console.log ( `OK: CODE [[new Serl]] RETURNED ${new Serl}` ) } 
+        } from './serl.js'
+
+console.log ( `TEST #1 : does the Node class / function / object exist?` )
+try { console.log ( `OK: CODE [[new Node instanceof Node]] RETURNED [[${new Node
+    instanceof Node}]]` ) } 
 catch (e) { console.error(e) } 
-finally {console.log(`--TEST ENDED--`)}
+finally {console.log('TEST ENDED\n-- ')}
+
+console.log ( `TEST #1.1 : do Node instances have names ?` )
+try { console.log ( `OK: CODE [[(new Node('tom')).name]] RETURNED [[${(new
+    Node('tom')).name}]]` ) } 
+catch (e) { console.error(e) } 
+finally {console.log('TEST ENDED\n-- ')}
+
+console.log ( `TEST #2 : does the Pid class / function / object exist?` )
+try { console.log ( `OK: CODE [[new Pid instanceof Pid]] RETURNED [[${new Pid
+instanceof Pid}]]` ) } 
+catch (e) { console.error(e) } 
+finally {console.log('TEST ENDED\n-- ')}
