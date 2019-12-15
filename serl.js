871ec1e40841ebf7a96a1cfaff54f583d14e3205
Jerng <yangjerng@gmail.com>
2019-12-16 05:52:42 +0800

    Implementing Node.spawn/1, Proc, and Pid

--- serl.js
+++ serl.js
@@ -6,10 +6,31 @@
 'use strict'
 
 // This is chosen over an (export default) expression for explicitness:
+//
 export class Node {
+    static nodeIndexFromNodeName (node, nodeName) {
+        return Array.from( node.nodeMap.keys() )
+                    .find( (key) => node.nodeMap.get(key).name == nodeName )
+    }
     constructor (given) {
         this.host   =   'placeholderHost'
         this.name   =   `${given}@${this.host}`   // TODO: depends on -name vs -sname flags
+
+        this.procMap    =   new Map( [ ] )
+        Object.defineProperty ( this.procMap, 'counter', {  value : 0,
+                                                            writable: true } )
+        // Making this un-writable would be safer, but more expensive as the
+        // value would have to BE RE-CONFIGURed for mutation; and we expect this
+        // to change frequently.
+
+        // Discuss: not using an Array, because we follow Erlang convention of
+        //          limiting the number of connected nodes in a lifetime; so
+        //          keys are the nodeIndex
+        //
+        //          Here we impose a convention where the local node is index=0
+        this.nodeMap    =   new Map(    [   [0, { name:this.name }] 
+                                        ] )
+
 
         /*  Distributed Erlang functionality (we don't need this yet) 
          *  https://erlang.org/doc/reference_manual/distributed.html
@@ -19,12 +40,73 @@ export class Node {
 
          */
 
-        this.spawn  = () => {}
     }
-}
+
+
+    // TODO: implement the 'undefined function' error
+    // TODO: refer to 'dynamic module loading' and module objects later 
+    spawn () {
+        let nodeName, procIndex, newProc
+        switch (arguments.length) {
+            case 0:
+                throw Error( 'Node.spawn/0 called; no implementation.' )
+                break
+
+            case 1:
+
+                //  http://erlang.org/doc/man/erlang.html#spawn-1
+                //  Expect a function.
+                if ( typeof arguments[0] != 'function' ) { 
+                    throw Error( 'Node.spawn/1 called with a non-function' ) 
+                }
+
+                nodeName    = this.name
+                procIndex   = ++this.procMap.counter
+                break
+
+            case 2:
+                throw Error( 'Node.spawn/2 called; no implementation.' )
+                break
+            case 3:
+                throw Error( 'Node.spawn/3 called; no implementation.' )
+                break
+            case 4:
+                throw Error( 'Node.spawn/4 called; no implementation.' )
+                break
+            case 5:
+                throw Error( 'Node.spawn/5 called; no implementation.' )
+                break
+            default:
+                throw Error( 'Node.spawn/(>5) called; no implementation.' )
+        }
+        newProc = new Proc ( nodeName, procIndex, this )
+        return newProc.pid
+
+    } // method Node.spawn
+
+} // class Node
+
+export class Proc {
+    // Dependence on localNode is questionable; TODO: review
+    constructor (nodeName, procIndex, localNode) {
+        // TODO: do we need to validate argument types here?
+        this.nodeIndex  = Node.nodeIndexFromNodeName ( localNode, nodeName)
+        this.pid = new Pid (this.nodeIndex, procIndex)
+    }
+    toString () {
+        return `[object Proc<${this.pid.nodeIndex}.${this.pid.procIndex}>]`
+    }
+} 
 
 export class Pid {
-    constructor () {
+    constructor (nodeIndex, procIndex) {
+        // TODO: do we need to validate argument types here?
+        this.nodeIndex  = nodeIndex
+        this.procIndex  = procIndex
     }
-}
+    toString () {
+        return `[object Pid<${this.nodeIndex}.${this.procIndex}>]`
+    }
+} 
 
+// TODO: export class Reference ()
--- tests.js
+++ tests.js
@@ -1,22 +1,137 @@
-import  {   Node, 
-            Pid
+import * as Serl from './serl.js'
 
-        } from './serl.js'
-
-console.log ( `TEST #1 : does the Node class / function / object exist?` )
-try { console.log ( `OK: CODE [[new Node instanceof Node]] RETURNED [[${new Node
-    instanceof Node}]]` ) } 
+console.log ( `[TEST #1 : does the Node class / function / object exist?` )
+try { console.log ( `OK: CODE [[new Serl.Node instanceof Serl.Node]] RETURNED
+    [[${new Serl.Node instanceof Serl.Node}]]` ) } 
 catch (e) { console.error(e) } 
-finally {console.log('TEST ENDED\n-- ')}
+finally {console.log('] - - ')}
 
-console.log ( `TEST #1.1 : do Node instances have names ?` )
-try { console.log ( `OK: CODE [[(new Node('tom')).name]] RETURNED [[${(new
-    Node('tom')).name}]]` ) } 
+console.log ( `[TEST #1.1 : do Node instances have names ?` )
+try { console.log ( `OK: CODE [[(new Serl.Node('tom')).name]] RETURNED [[${(new
+    Serl.Node('tom')).name}]]` ) } 
 catch (e) { console.error(e) } 
-finally {console.log('TEST ENDED\n-- ')}
+finally {console.log('] - - ')}
 
-console.log ( `TEST #2 : does the Pid class / function / object exist?` )
-try { console.log ( `OK: CODE [[new Pid instanceof Pid]] RETURNED [[${new Pid
-instanceof Pid}]]` ) } 
+console.log ( `[TEST #1.2 : does static method Pid.nodeIndexFromNodeName()
+    work?` ); try { console.log ( `OK: CODE [[  
+        let n = new Serl.Node ('test1.2')
+        return Serl.Node.nodeIndexFromNodeName ( n, n.name )
+    ]] RETURNED [[${(()=>{
+        let n = new Serl.Node ('test1.2')
+        return Serl.Node.nodeIndexFromNodeName ( n, n.name )
+})() }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+console.log ( `[TEST #2 : does the Pid class / function / object exist?` )
+try { console.log ( `OK: CODE [[
+        new Serl.Pid (  'placeholderNodeIndex', 
+                        'placeholderProcIndex' ) instanceof Serl.Pid
+    ]] RETURNED [[${
+        new Serl.Pid (  'placeholderNodeIndex', 
+                        'placeholderProcIndex' ) instanceof Serl.Pid
+    }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+console.log ( `[TEST #2.1 : how readable is the Pid's .toString()?` ); try {
+    console.log ( `OK: CODE [[ 
+        new Serl.Pid ( 'placeholderNodeName', 'placeholderProcIndex' )
+    ]] RETURNED [[${(()=>{
+        let n = new Serl.Node ('test2.1')
+        return new Serl.Pid ( 'placeholderNodeName', 'placeholderProcIndex' )
+})() }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+console.log ( `[TEST #3 : spawn(()=>{})?` )
+try { console.log ( `OK: CODE [[
+        (new Serl.Node).spawn(()=>{})
+    ]] RETURNED [[
+        ${(new Serl.Node).spawn(()=>{})}
+    ]]` ) } 
 catch (e) { console.error(e) } 
-finally {console.log('TEST ENDED\n-- ')}
+finally {console.log('] - - ')}
+
+console.log ( `[TEST #3.1 : spawn(1)?` )
+try { console.error ( `NO ERROR THROWN: CODE [[ 
+        (new Serl.Node).spawn(1)
+    ]] RETURNED [[
+        ${(new Serl.Node).spawn(1)}
+    ]]` ) } 
+catch (e) { console.log(`OK: CODE [[
+        (new Serl.Node).spawn(1)
+    ]] RETURNED [[
+        ${( e instanceof Error) 
+                ? e 
+                : new Error('Expected an error, did not obtain one.')}
+    ]]`) } 
+finally {console.log('] - - ')}
+
+console.log ( `[TEST #3.2 : do successive calls to spawn() increment the Proc
+Pids in the Node's procMap?` ); try { console.log ( `OK: CODE [[  
+        let n = new Serl.Node ('test3.2')
+        return [n.spawn(()=>{}), n.spawn(()=>{})]
+    ]] RETURNED [[${(()=>{
+        let n = new Serl.Node('test3.2')
+        return [n.spawn(()=>{}), n.spawn(()=>{})]
+})() }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+console.error ( `[TEST #3.n : tests for arities != 1, unwritten at this time.]` )
+
+console.log ( `[TEST #4 : Node's procMap is readable ?` )
+try { console.log ( `OK: CODE [[ (new Serl.Node).procMap.counter ]] RETURNED
+    [[${ (new Serl.Node).procMap.counter }]]` ) } 
+catch (e) { console.error(e) } 
+finally {console.log('] - - ')}
+
+console.log ( `[TEST #4.1 : Node's procMap is writable ?` )
+try { console.log ( `OK: CODE [[  
+        let n = (new Serl.Node)
+        n.procMap.counter += 1
+        return n.procMap.counter
+    ]] RETURNED [[${(()=>{
+        let n = (new Serl.Node)
+        n.procMap.counter += 1
+        return n.procMap.counter
+    })() }]]` ) } 
+catch (e) { console.error(e) } 
+finally {console.log('] - - ')}
+
+console.log ( `[TEST #5 : does the Proc class / function / object exist?` )
+try { console.log ( `OK: CODE [[
+        new Serl.Proc ( 'placeholderNodeName', 
+                        'placeholderProcIndex', 
+                        new Serl.Node ('test5') ) instanceof Serl.Proc
+    ]] RETURNED [[${
+        new Serl.Proc ( 'placeholderNodeName', 
+                        'placeholderProcIndex', 
+                        new Serl.Node ('test5') ) instanceof Serl.Proc
+    }]]` ) } 
+catch (e) { console.error(e) } 
+finally {console.log('] - - ')}
+
+console.log ( `[TEST #5.1 : how readable is the Proc's .toString()?` ); try { console.log ( `OK: CODE [[  
+        let n = new Serl.Node ('test5')
+        return new Serl.Proc ( n.name, 'placeholderProcIndex', n ) 
+    ]] RETURNED [[${(()=>{
+        let n = new Serl.Node ('test5')
+        return new Serl.Proc ( n.name, 'placeholderProcIndex', n ) 
+})() }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+/* template test (NO-expected-error):
+
+console.log ( `[TEST # : ?` ); try { console.log ( `OK: CODE [[  
+
+    ]] RETURNED [[${(()=>{
+
+        return x
+})() }]]` ) } catch (e) { console.error(e) } finally {console.log('] - - ')}
+
+*/
+/* template test (expected-error):
+
+console.log ( `[TEST # : ?` ) try { console.error ( `NO ERROR THROWN: CODE [[ 
+
+    ]] RETURNED [[
+
+    ]]` ) } catch (e) { console.log(`OK: CODE [[
+
+    ]] RETURNED [[ ${( e instanceof Error) ? e : new Error ('Expected an error,
+did not obtain one.')} ]]`) } finally {console.log('] - - ')}
+
+*/
